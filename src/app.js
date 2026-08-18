f8f79a3df9172c76c93f3494611d8f950f5798c6
aries510 <darenrespicio@gmail.com>
2026-08-18 16:00:52 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 8081;
+
+const knex = require('knex')(require('../knexfile.js')['development']);
+
+app.get('/', (request, response) => {
+    response.send('Application running.')
+});
+
+app.get('/pets', (request, response) => {
+    knex('pet')
+        .select('*')
+        .then(pets => {
+            var petNames = pets.map(pet => pet.name)
+            response.json(petNames)
+        })
+})
+
+app.listen(port, () => {
+    console.log('Your KNEX and Express Application are running succesfully')
+});
